0879e359da416de3d940800120a5cd2da7d67038
gbrlbsls <gabrielbsales@gmail.com>
2023-10-01 19:08:42 -0300

    initial commit

---
+++ wtlcrud/src/main/java/com/devgbrl/wtlcrud/WtlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.devgbrl.wtlcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtlCrudApplication.class, args);
+	}
+
+}
---
+++ wtlcrud/src/test/java/com/devgbrl/wtlcrud/WtlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgbrl.wtlcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
